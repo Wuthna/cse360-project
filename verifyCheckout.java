b7045503958716d8bcd705a3bd7abe1b7692132f
CCMiles32 <70864211+CCMiles32@users.noreply.github.com>
2022-10-23 14:08:17 -0700

    Customer is mostly done except for the enter student ID

--- verifyCheckout.java
+++ verifyCheckout.java
@@ -1,7 +1,8 @@
 //package GUI;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionListener;
-
+import java.awt.*;
 import javax.swing.*;
 
 //import 
@@ -12,23 +13,32 @@ public class verifyCheckout extends cartPage implements ActionListener{
 
     JButton enterButton = new JButton();
     JLabel asuIDTitle = new JLabel();
+    JTextField studentIDTextBox = new JTextField(10);
 
     verifyCheckout() {
     this.dispose();
         System.out.println("Verify Checkout");
     
-
-
         enterButton.addActionListener(this);
         enterButton.setText("Enter");
         enterButton.setBounds(150,250, 400, 100);
 
         asuIDTitle.setText("Enter ASU ID");
         asuIDTitle.setFont(new java.awt.Font("Serif", java.awt.Font.PLAIN, 30));
-        asuIDTitle.setForeground(Color.WHITE);
-        asuIDTitle.setBounds(30, 9, 100, 30);
+        asuIDTitle.setForeground(SunDevilBlack);
+        asuIDTitle.setBounds(30, 9, 300, 30);
 
 
+        studentIDTextBox.setPreferredSize(new Dimension(100,50));
+        studentIDTextBox.setForeground(SunDevilSilver);
+        
+        //studentIDTextBox.setText("ASU ID");
+        //studentIDTextBox.setBackground(SunDevilMaroon);
+
+
+        verifyCheckout.add(enterButton);
+        verifyCheckout.add(asuIDTitle);
+        verifyCheckout.add(studentIDTextBox);
         verifyCheckout.setVisible(true); //makes frame visible
         verifyCheckout.setTitle("Checkout"); //sets title
         verifyCheckout.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //sets exit to close app
@@ -36,9 +46,8 @@ public class verifyCheckout extends cartPage implements ActionListener{
         verifyCheckout.setSize(700,400); //setse starting dimentions
         verifyCheckout.setLocationRelativeTo(null); //When opened, opens in center of screen
         verifyCheckout.getContentPane().setBackground(SunDevilGold);
-        verifyCheckout.add(enterButton);
 
-        verifyCheckout.getContentPane().setLayout(null);
+        verifyCheckout.getContentPane().setLayout(null); //absolute positions
         
     }
 
@@ -47,7 +56,7 @@ public class verifyCheckout extends cartPage implements ActionListener{
     public void actionPerformed(java.awt.event.ActionEvent e) {
     
         if(e.getSource() == enterButton) {
-            
+            System.out.println("HERE" + studentIDTextBox.getText());
             waitTimePage newWaitTime = new waitTimePage();
             verifyCheckout.dispose();
             cartPage.dispose();
--- waitTimePage.java
+++ waitTimePage.java
@@ -9,11 +9,24 @@ import javax.swing.*;
 public class waitTimePage extends cartPage implements ActionListener{
 
     static JFrame waitTimePage = new JFrame();
+    JLabel acceptedOrderLabel = new JLabel();
+    JLabel waitTimeLabel = new JLabel();
+    int estWaitTime;
 
     waitTimePage() {
 
+        acceptedOrderLabel.setText("Your Order Has Been Accepted");
+        acceptedOrderLabel.setFont(new java.awt.Font("Serif", java.awt.Font.PLAIN, 30));
+        acceptedOrderLabel.setForeground(Color.WHITE);
+        acceptedOrderLabel.setBounds(30, 9, 500, 50);
+         
+        waitTimeLabel.setText("Estimated Wait time is " + estWaitTime + " minutes");
+        waitTimeLabel.setFont(new java.awt.Font("Serif", java.awt.Font.PLAIN, 30));
+        waitTimeLabel.setForeground(Color.WHITE);
+        waitTimeLabel.setBackground(SunDevilMaroon);
+        waitTimeLabel.setOpaque(true);
+        waitTimeLabel.setBounds(125, 200, 400, 80);
 
-        
         waitTimePage.setVisible(true); //makes frame visible
         waitTimePage.setTitle("Checkout"); //sets title
         waitTimePage.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //sets exit to close app
@@ -21,7 +34,8 @@ public class waitTimePage extends cartPage implements ActionListener{
         waitTimePage.setSize(700,400); //setse starting dimentions
         waitTimePage.setLocationRelativeTo(null); //When opened, opens in center of screen
         waitTimePage.getContentPane().setBackground(SunDevilBlack);
-        //waitTimePage.add();
+        waitTimePage.add(acceptedOrderLabel);
+        waitTimePage.add(waitTimeLabel);
 
         waitTimePage.getContentPane().setLayout(null);
     }
