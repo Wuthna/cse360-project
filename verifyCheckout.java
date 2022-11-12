9ef9dcaa6cfa70eca4ca26484fa96607f5f70bfe
Adrian Mena Altamirano <79997453+Wuthna@users.noreply.github.com>
2022-11-12 13:01:18 -0700

    Update verifyCheckout.java
    - added comments to make code readable 
    - fixed issue where estimated wait time was not displaying

--- verifyCheckout.java
+++ verifyCheckout.java
@@ -10,46 +10,41 @@ import javax.swing.JTextField;
 
 public class verifyCheckout extends cartPage implements ActionListener{
 
+	//initializing variables
     static JFrame verifyCheckout = new JFrame();
-
     JButton enterButton = new JButton();
     JLabel asuIDTitle = new JLabel();
-    // JTextField studentIDTextBox = new JTextField(10);
     JTextField studentIDTextBox;
+    String order; 
+    int EWT = 0;
     
-
-    verifyCheckout() {
-    //this.dispose();
-
+    verifyCheckout(int estWaitTime) {
+    	//initializing and declaring variables
+    	EWT = estWaitTime;
         studentIDTextBox = new JTextField();
         System.out.println("Verify Checkout");
     
+        //declaring enter button properties
         enterButton.addActionListener(this);
         enterButton.setText("Enter");
         enterButton.setBounds(150,250, 400, 100);
 
+        //declaring ASU ID title properties
         asuIDTitle.setText("Enter ASU ID");
         asuIDTitle.setFont(new java.awt.Font("Serif", java.awt.Font.PLAIN, 30));
         asuIDTitle.setForeground(SunDevilBlack);
         asuIDTitle.setBounds(30, 9, 300, 30);
 
-
         //initialize the text box
         studentIDTextBox.setBounds(30, 50, 300, 30);
         studentIDTextBox.setFont(new java.awt.Font("Serif", java.awt.Font.PLAIN, 30));
         studentIDTextBox.setForeground(SunDevilBlack);
-        
         studentIDTextBox.setCaretColor(SunDevilBlack);
-
         //studentIDTextBox.setForeground(SunDevilSilver);
-        
         //studentIDTextBox.setText("ASU ID");
         //studentIDTextBox.setBackground(SunDevilMaroon);
 
-
-        // verifyCheckout.add(enterButton);
-        // verifyCheckout.add(asuIDTitle);
-        // verifyCheckout.add(studentIDTextBox);
+        /*setting the scene and adding contents onto verify checkout JFrame */
         verifyCheckout.setVisible(true); //makes frame visible
         verifyCheckout.setTitle("Checkout"); //sets title
         verifyCheckout.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //sets exit to close app
@@ -57,29 +52,22 @@ public class verifyCheckout extends cartPage implements ActionListener{
         verifyCheckout.setSize(700,400); //setse starting dimentions
         verifyCheckout.setLocationRelativeTo(null); //When opened, opens in center of screen
         verifyCheckout.getContentPane().setBackground(SunDevilGold);
-
         verifyCheckout.getContentPane().setLayout(null); //absolute positions
         verifyCheckout.add(enterButton);
         verifyCheckout.add(asuIDTitle);
         verifyCheckout.add(studentIDTextBox);
     }
-
-
+    //display the estimated wait time page when the enter button is clicked
     @Override
     public void actionPerformed(java.awt.event.ActionEvent e) {
     
         if(e.getSource() == enterButton) {
             System.out.println("HERE" + studentIDTextBox.getText());
-            waitTimePage newWaitTime = new waitTimePage();
+            //pass on estimated wait time to ensure it is not lost when a new home page instance is declared 
+            waitTimePage newWaitTime = new waitTimePage(this.EWT);
             verifyCheckout.dispose();
             cartPage.dispose();
             this.dispose();
         }
-    
     }
-
-
-
-
-
 }
