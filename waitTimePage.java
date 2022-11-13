5d38ebf3c9328e4c63eea5f78156f174966695d3
Adrian Mena Altamirano <79997453+Wuthna@users.noreply.github.com>
2022-11-12 19:52:51 -0700

    Update waitTimePage.java
    - overhauled GUI

--- waitTimePage.java
+++ waitTimePage.java
@@ -1,5 +1,7 @@
 //package GUI;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
@@ -8,20 +10,33 @@ public class waitTimePage extends cartPage implements ActionListener{
     static JFrame waitTimePage = new JFrame();
     JLabel acceptedOrderLabel = new JLabel();
     JLabel waitTimeLabel = new JLabel();
+    JLabel titleLabel = new JLabel("Wait Time Page");
+    JLabel asuImage3 = new JLabel(new ImageIcon(new ImageIcon(getClass().getResource("asu.png")).getImage().getScaledInstance(175, 75, Image.SCALE_DEFAULT)));
 
     waitTimePage(int estWaitTime) {
+    	//title properties
+    	asuImage3.setBounds(0, 0, 175, 75);
+    	asuImage3.setBackground(Color.WHITE);
+    	asuImage3.setOpaque(true);
+    	
+    	titleLabel.setBounds(175, 0, 525, 75);
+    	titleLabel.setFont(new java.awt.Font(Font.SANS_SERIF, java.awt.Font.BOLD, 40));
+    	titleLabel.setBackground(Color.white);
+    	titleLabel.setForeground(SunDevilBlack);
+    	titleLabel.setOpaque(true);
+    	
     	//declaring variables properties
-        acceptedOrderLabel.setText("Your Order Has Been Accepted");
-        acceptedOrderLabel.setFont(new java.awt.Font("Serif", java.awt.Font.PLAIN, 30));
-        acceptedOrderLabel.setForeground(Color.WHITE);
-        acceptedOrderLabel.setBounds(30, 9, 500, 50);
+        acceptedOrderLabel.setText("Your order has been accepted");
+        acceptedOrderLabel.setFont(new java.awt.Font("Serif", java.awt.Font.PLAIN, 25));
+        acceptedOrderLabel.setBounds(125, 150, 350, 30);
          
-        waitTimeLabel.setText("Estimated Wait time is " + estWaitTime + " minutes");
-        waitTimeLabel.setFont(new java.awt.Font("Serif", java.awt.Font.PLAIN, 30));
+        waitTimeLabel.setText("Estimated wait time is " + estWaitTime + " minutes");
+        waitTimeLabel.setFont(new java.awt.Font("Serif", java.awt.Font.PLAIN, 25));
         waitTimeLabel.setForeground(Color.WHITE);
         waitTimeLabel.setBackground(SunDevilMaroon);
+        waitTimeLabel.setBorder(blackBrdr);
         waitTimeLabel.setOpaque(true);
-        waitTimeLabel.setBounds(125, 200, 400, 80);
+        waitTimeLabel.setBounds(125, 200, 350, 30);
 
         //setting the scene and adding content to the JFrame
         waitTimePage.setVisible(true); //makes frame visible
@@ -30,9 +45,11 @@ public class waitTimePage extends cartPage implements ActionListener{
         waitTimePage.setResizable(false); //makes GUI ajustable
         waitTimePage.setSize(700,400); //setse starting dimentions
         waitTimePage.setLocationRelativeTo(null); //When opened, opens in center of screen
-        waitTimePage.getContentPane().setBackground(SunDevilBlack);
+        waitTimePage.getContentPane().setBackground(SunDevilGold);
         waitTimePage.add(acceptedOrderLabel);
         waitTimePage.add(waitTimeLabel);
+        waitTimePage.add(titleLabel);
+        waitTimePage.add(asuImage3);
         //ensure contents stay in their fixed position 
         waitTimePage.getContentPane().setLayout(null);
     }
