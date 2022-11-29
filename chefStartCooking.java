245628d5f6b386b3e3d1b49be74b6d58229edaad
Adrian Mena Altamirano <79997453+Wuthna@users.noreply.github.com>
2022-11-29 11:53:39 -0700

    Update chefStartCooking.java
    -updated the front end

--- chefStartCooking.java
+++ chefStartCooking.java
@@ -1,4 +1,6 @@
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
@@ -10,25 +12,45 @@ public class chefStartCooking extends chefPage implements ActionListener {
     JButton backButton = new JButton();
     JCheckBox pizzaCheckBox = new JCheckBox();
     JLabel chefTitle = new JLabel();
+    JLabel asuImage5 = new JLabel(new ImageIcon(new ImageIcon(getClass().getResource("asu.png")).getImage().getScaledInstance(175, 75, Image.SCALE_DEFAULT)));
+    JLabel menuTitle = new JLabel("SunDevil Pizza", JLabel.LEFT);
+    
     chefStartCooking() {
 
         this.dispose();
-
-        chefTitle.setText("Pizza status for order set to COOKING");
-        chefTitle.setFont(new java.awt.Font("Serif", java.awt.Font.PLAIN, 20));
+        //title image and text
+        asuImage5.setBounds(0,0,200,50);
+        asuImage5.setBackground(Color.WHITE);
+        asuImage5.setOpaque(true);
+        menuTitle.setBounds(200,0,500,50);
+        menuTitle.setFont(new java.awt.Font(Font.SANS_SERIF, java.awt.Font.BOLD, 40));
+        menuTitle.setBackground(Color.WHITE);
+        menuTitle.setOpaque(true);
+        
+        //chef title
+        chefTitle.setText("Finished Orders");
+        chefTitle.setFont(new java.awt.Font(Font.SANS_SERIF, java.awt.Font.PLAIN, 20));
         chefTitle.setForeground(Color.WHITE);
-        chefTitle.setBounds(30, 9, 460, 30);
+        chefTitle.setBackground(SunDevilBlack);
+        chefTitle.setBorder(blackBrdr);
+        chefTitle.setOpaque(true);
+        chefTitle.setBounds(50, 100, 600, 30);
 
-
-        startCookingButton.setBounds(300, 200, 200, 80);
+        //pizza finished button
+        startCookingButton.setBounds(300, 300, 100, 30);
         startCookingButton.addActionListener(this);
-        startCookingButton.setText("click when pizza is finished");
+        startCookingButton.setText("Finished");
+        startCookingButton.setBackground(SunDevilGold);
+        startCookingButton.setForeground(SunDevilBlack);
+        startCookingButton.setBorder(blackBrdr);
 
-        pizzaCheckBox.setBounds(150, 150, 420, 50);
+        //order list
+        pizzaCheckBox.setBounds(50, 130, 600, 50);
         pizzaCheckBox.addActionListener(this);
         pizzaCheckBox.setText("ORDER NUMBER 1 ");
         pizzaCheckBox.setFocusable(false);
-        pizzaCheckBox.setFont(new java.awt.Font("Serif", java.awt.Font.PLAIN, 15));
+        pizzaCheckBox.setBackground(Color.WHITE);
+        pizzaCheckBox.setFont(new java.awt.Font(Font.SANS_SERIF, java.awt.Font.PLAIN, 12));
 
         chefStartCooking.setVisible(true); // makes frame visible
         chefStartCooking.setTitle("Cooking view"); // sets title
@@ -40,6 +62,8 @@ public class chefStartCooking extends chefPage implements ActionListener {
         chefStartCooking.add(startCookingButton);
         chefStartCooking.add(backButton);
         chefStartCooking.add(chefTitle);
+        chefStartCooking.add(menuTitle);
+        chefStartCooking.add(asuImage5);
         chefStartCooking.add(pizzaCheckBox);
         chefStartCooking.getContentPane().setLayout(null); // absolute position
     }
